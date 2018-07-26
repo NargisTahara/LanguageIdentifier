0414e0f1a6d0159e0dc95be0f1e47962e5ff3be9
NargisTahara <nargistahara@gmail.com>
2018-07-26 14:38:33 -0700

    Dutch, German and Danish added!

--- src/com/collection/languageidentifier/IdentifierFunctions.java
+++ src/com/collection/languageidentifier/IdentifierFunctions.java
@@ -3,7 +3,7 @@ package com.collection.languageidentifier;
 
 public class IdentifierFunctions {
 	
-	static Object[][] allLanguages = {{"English",0}, {"Italian",0}, {"French",0}, {"Spanish",0}};
+	static Object[][] allLanguages = {{"English",0}, {"Italian",0}, {"French",0}, {"Spanish",0}, {"Dutch",0}, {"German",0}, {"Danish",0}};
 	
 	static int englishScore = 0;
 	static int italianScore = 0;
@@ -13,10 +13,13 @@ public class IdentifierFunctions {
 	//Identify language with highest score 
 	public static String languageIdentifier(String content) {
 		
-		allLanguages[0][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.englishCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.englishSuffixes) + LetterSequenceScoreCalculator.LetterSequenceScore(content,LanguageRules.englishLetterSequences);
-		allLanguages[1][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.italianCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.italianSuffixes) + LetterSequenceScoreCalculator.LetterSequenceScore(content,LanguageRules.italianLetterSequences);
-		allLanguages[2][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.frenchCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.frenchSuffixes) + LetterSequenceScoreCalculator.LetterSequenceScore(content,LanguageRules.frenchLetterSequences);
-		allLanguages[3][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.spanishCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.spanishSuffixes) + LetterSequenceScoreCalculator.LetterSequenceScore(content,LanguageRules.spanishLetterSequences);
+		allLanguages[0][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.englishCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.englishSuffixes);
+		allLanguages[1][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.italianCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.italianSuffixes);
+		allLanguages[2][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.frenchCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.frenchSuffixes);
+		allLanguages[3][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.spanishCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.spanishSuffixes);
+		allLanguages[4][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.dutchCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.dutchSuffixes);
+		allLanguages[5][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.germanCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.germanSuffixes);
+		allLanguages[6][1] = CommonWordsScoreCalculator.CommonWordsScore(content,LanguageRules.danishCommonWords) + SuffixScoreCalculator.SuffixScore(content,LanguageRules.danishSuffixes);
 		
 		//Temporary
 		for(int i = 0; i < allLanguages.length; i++)
--- src/com/collection/languageidentifier/LanguageRules.java
+++ src/com/collection/languageidentifier/LanguageRules.java
@@ -4,23 +4,36 @@ package com.collection.languageidentifier;
 
 public class LanguageRules {
 	
-	//English Language
+	// 1. English Language
 	public static String englishCommonWords[] = {"a","an","and","in","of","on","the","that","to","is","I","be","have","it","for","not","with","as","do","this"};
 	public static String englishSuffixes[] = {"ing","tion","ed","age","'s","'ve","n't","'d","al","acy","dom","ist","ity","ate","en","ify","ize","ful","ic","ive"};
 	public static String englishLetterSequences[] = {"th", "ch", "sh", "er", "re", "ed", "ha", "edt", "tis", "oft"};
 	
-	//Italian Language
+	// 2. Italian Language
 	public static String italianCommonWords[] = {"è","perché","come","io","che","lui","era","per","su","sono","con","essi","uno","avere","questo","da","caldo","ma","cosa","alcuni"};
 	public static String italianSuffixes[] = {"aia","ento","tà","ggio","aio","ista","tore","ccio","stro","ano","ese","ata","ento","tura","ei","etto","ino","one","eria","ere"};
 	public static String italianLetterSequences[] = {"gli","gn","sci","tt","zz","cc","ss","bb","pp","ll"};
 	
-	//French Language
+	// 3. French Language
 	public static String frenchCommonWords[] = {"de","la","le","du","des","il","et","un","à","être","en","que","pour","ce","qui","se","pas","plus","par","je"};
 	public static String frenchSuffixes[] = {"ux","acé","ain","ât","âtre","é","ème","erai","fère","gène","ière","is","ît","man","mont","oche","oir","ot","quin","ïsme"};
 	public static String frenchLetterSequences[] = {"ph","ch","xc","aî","eû","ey","oe","üe","ain","cqu"};
 	
-	//Spanish Language
+	// 4. Spanish Language
 	public static String spanishCommonWords[] = {"de","el","que","del","los","la","uno","una","y","los","por","con","sobre","también","había","así","él","después","sí","país"};
 	public static String spanishSuffixes[] = {"ción","ento","dad","aba","áis","anza","aré","ceta","ción","lasa","ear","eda","eña","eré","ez","fila","geno","ías","illa","lito"};
 	public static String spanishLetterSequences[] = {"ch","ll","gu","qu","rr","ch","ll","gu","qu","rr"};
+	
+	// 5. Dutch Language
+	public static String dutchCommonWords[] = {"het","op","en","een","voor","ik","je","dat","niet","wat","van","ze","te","zijn","maar","ben","mijn","dit","aan","om"};
+	public static String dutchSuffixes[] = {"tje","sje","ing","en","lijk","aar","aat","ares","ator","baar","der","dom","eur","ief","iek","ist","jes","kje","loog","meer"};
+	public static String dutchLetterSequences[] = {"ij","ei","kw","sch","oei","ooi","uw","aai","auw","ieuw" };
+	
+	// 6. German Language
+	public static String germanCommonWords[] = {"der","die","das","den","dem","des","er","sie","es","ist","ich","du","aber","und","wir","zu","ein","mir","daß","haben"};
+	public static String germanSuffixes[] = {"en","er","ern","st","ung","chen","tät","sch","eln","erei","graf","iert","ös","phil","sel","thum","trop","ung","voll","witz"};
+	
+	// 7. Danish Language
+	public static String danishCommonWords[] = {"af","og","til","er","på","med","det","den","ikke","som","han","vi","fra","skal","også","eller","være","blev","når","noget"};
+	public static String danishSuffixes[] = {"tion","ing","else","hed","bar","boer","eri","fobi","inde","itet","lyse","løs","skab","som","syre","trice","vis","yn","årig","ær"};
 }
